2d54c4c5db7fd02a8e4d5649aacf7a1c22dd8555
Daniel Coutinho <36049697+danmrc@users.noreply.github.com>
2026-01-14 23:49:05 -0500

    add lagrange multiplier to foc to nlsolve and reparametrizes

--- hw2/attempt1.jl
+++ hw2/attempt1.jl
@@ -28,7 +28,7 @@ end
 struct solution_cons
     assets::Array
     consumption::Array
-    op::Optim.MultivariateOptimizationResults
+    op
 end
 
 function solve_consumer(ys,a0,r,β,κ,γ,a_bar,b_bar,T)
@@ -46,13 +46,25 @@ end
 
 foc(c,cp,γ,β,κ = 1) = uprime(c,γ) - β*κ*uprime(cp,γ)
 
-function build_focs(as,ys,r,β,κ,γ,a_bar,b_bar,T)
+function build_focs(as,ys,r,β,κ,γ,a_bar,μ,T)
 
+    as[T+1] = exp(as[T+1])
     cs = map(t->(1+r)*as[t] + ys[t] - as[t+1],1:T)
     focs_alive = map((c,cp)->foc(c,cp,γ,β),cs[1:(T-1)],cs[2:T])
-    foc_dead = foc(cs[T],as[T+1] + a_bar,γ,β,κ)
+    foc_dead = foc(cs[T],as[T+1] + a_bar,γ,β,κ) + μ
 
-    return [focs_alive;foc_dead]
+    return [focs_alive;foc_dead;μ*as[T+1]]
+
+end
+
+function solve_consumer2(ys,a0,r,β,κ,γ,a_bar,b_bar,T)
+
+    sol = nlsolve(a->build_focs([a0;a[1:T]],ys,r,β,κ,γ,a_bar,a[T+1],T),fill(0.01,T+1), autodiff = :forward)
+    assets = [a0;sol.zero[1:(T+1)]]
+    assets[T+1] = exp(assets[T+1])
+    consumption = (1+r)*assets[1:T] + ys - assets[2:(T+1)]
+
+    return solution_cons(assets,consumption,sol)
 
 end
 
@@ -67,9 +79,11 @@ T = 5
 ys = collect(1:T)
 
 tt = solve_consumer(ys,0,r,β,κ,γ,a_bar,b_bar,T)
+tt2 = solve_consumer2(ys,0,r,β,κ,γ,a_bar,b_bar,T)
+
 
 as = zeros(T+1)
-as[1] = 0
+as[1] = 1
 
 for t = 1:5
 
@@ -77,9 +91,9 @@ for t = 1:5
 
 end
 
-build_focs(as,ys,r,β,0.3,1,a_bar,b_bar,T)
+build_focs(as,ys,r,β,0,1,a_bar,b_bar,T)
 
-nl_sol = nlsolve(a->build_focs([0;a],ys,r,β,0.3,1,a_bar,b_bar,T),fill(0.01,T), autodiff = :forward)
+nl_sol = nlsolve(a->build_focs([0;a[1:T]],ys,r,β,0,1,a_bar,a[T+1],T),fill(0.01,T+1), autodiff = :forward)
 nl_sol2 = mcpsolve(a->build_focs([0;a],ys,r,β,0.3,1,a_bar,b_bar,T),lw,up,fill(0.01,T),autodiff = :forward)
 
 
